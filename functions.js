4a49ca3e546b4df9a9272f9e53bebd99f282d95c
39D■ <hpoje@icloud.com>
2018-04-08 00:34:42 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -11,14 +11,14 @@ function playAudio() {
 		document.getElementById('radio').src='img/radioOn.gif';
 		document.getElementById('pt').innerHTML = 'play &#9658';
 		if(music.currentTime >= 0 && music.currentTime < 118){
-			document.getElementById('artist-name').innerHTML = 'eery - her';}
-		else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain';}
-		else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee';}
-		else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms';}
-		else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)';}
-		else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)';}
-		else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)';}
-		else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white';}
+			document.getElementById('artist-name').innerHTML = 'eery - her'; document.getElementById('artist_link').href = "https://eery.bandcamp.com/album/her-ep";}
+		else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain'; document.getElementById('artist_link').href = "https://inneroceanrecords.bandcamp.com/track/bunko-stew-hide-the-pain";}
+		else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee'; document.getElementById('artist_link').href = "https://elijahwho.bandcamp.com/track/sad-and-boujee";}
+		else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms'; document.getElementById('artist_link').href = "https://saibeats.bandcamp.com/track/in-your-arms";}
+		else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)'; document.getElementById('artist_link').href = "https://nohidea.bandcamp.com/track/thirteen-w-idealism";}
+		else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)'; document.getElementById('artist_link').href = "https://rapperatlas.bandcamp.com/track/sand-prod-tomppabeats";}
+		else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)'; document.getElementById('artist_link').href = "https://soundcloud.com/potsupotsu/im-closing-my-eyes";}
+		else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white'; document.getElementById('artist_link').href = "https://eery.bandcamp.com/track/off-white";}
 		
 		
 	} else {
@@ -63,14 +63,14 @@ function isPlaying(music) { return !music.paused; }
 
 music.ontimeupdate = function(){
 	if(music.currentTime >= 0 && music.currentTime < 118){
-			document.getElementById('artist-name').innerHTML = 'eery - her';}
-	else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain';}
-	else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee';}
-	else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms';}
-	else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)';}
-	else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)';}
-	else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)';}
-	else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white';}
+			document.getElementById('artist-name').innerHTML = 'eery - her'; document.getElementById('artist_link').href = "https://eery.bandcamp.com/album/her-ep";}
+	else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain'; document.getElementById('artist_link').href = "https://inneroceanrecords.bandcamp.com/track/bunko-stew-hide-the-pain";}
+	else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee'; document.getElementById('artist_link').href = "https://elijahwho.bandcamp.com/track/sad-and-boujee";}
+	else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms'; document.getElementById('artist_link').href = "https://saibeats.bandcamp.com/track/in-your-arms";}
+	else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)'; document.getElementById('artist_link').href = "https://nohidea.bandcamp.com/track/thirteen-w-idealism";}
+	else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)'; document.getElementById('artist_link').href = "https://rapperatlas.bandcamp.com/track/sand-prod-tomppabeats";}
+	else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)'; document.getElementById('artist_link').href = "https://soundcloud.com/potsupotsu/im-closing-my-eyes";}
+	else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white'; document.getElementById('artist_link').href = "https://eery.bandcamp.com/track/off-white";}
 };
 
 music.onended = function() {
@@ -84,15 +84,3 @@ function changeImg(img){
 	img.src = "img/cassette-out.png";
 }
 
-
-// lamp light switch
-/*
-
-$('#lamp-button').on('click',function(){
-    if($('#lamp').css('display')!='none'){
-    $('#lamp-light').show().hide();
-    }else if($('#lamp-light').css('display')!='none'){
-        $('#lamp').show().hide();
-    }
-});
-*/
